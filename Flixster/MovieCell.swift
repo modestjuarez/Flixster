7ca315892c65507e261e2730708fcf88080072de
Modest J <modestjuarez@gmail.com>
2020-09-25 23:54:07 -0700

    Committing final files of my app

---
+++ Flixster/MovieCell.swift
@@ -0,0 +1,27 @@
+//
+//  MovieCell.swift
+//  Flixster
+//
+//  Created by Modest Juarez on 9/25/20.
+//  Copyright © 2020 Modest Juarez. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var synopsisLabel: UILabel!
+    @IBOutlet weak var posterView: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
