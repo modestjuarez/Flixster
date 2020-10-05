90049700f89945f8161154d44ff854d514365042
Modest J <modestjuarez@gmail.com>
2020-10-04 23:36:45 -0700

    Part 2 of the Assignment. Created the superhero view controller

---
+++ Flixster/MovieGridCell.swift
@@ -0,0 +1,15 @@
+//
+//  MovieGridCell.swift
+//  Flixster
+//
+//  Created by Modest Juarez on 10/3/20.
+//  Copyright © 2020 Modest Juarez. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
